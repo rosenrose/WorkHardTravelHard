49fb248f4ca2cabb420a156e455b1dbf9f1640c6
rosenrose <crazytempler@gmail.com>
2022-03-29 16:07:54 +0900

    textinput

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,18 +8,35 @@ import {
   TouchableHighlight,
   TouchableWithoutFeedback,
   Pressable,
+  TextInput,
 } from "react-native";
 import { theme } from "./colors";
 
 export default function App() {
+  const [isWork, setIsWork] = useState(true);
+  const work = () => {
+    setIsWork(true);
+  };
+  const travel = () => {
+    setIsWork(false);
+  };
+
+  const [text, setText] = useState("");
+  const onChangeText = (input) => {
+    setText(input);
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity activeOpacity={0.5}>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work} activeOpacity={0.5}>
+          <Text style={{ ...styles.btnText, color: isWork ? "white" : theme.gray }}>Work</Text>
         </TouchableOpacity>
-        <TouchableHighlight onPress={() => {}} underlayColor="red" activeOpacity={0.3}>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{ ...styles.btnText, color: isWork ? theme.gray : "white" }}>Travel</Text>
+        </TouchableOpacity>
+        {/* <TouchableHighlight onPress={() => {}} underlayColor="red" activeOpacity={0.3}>
           <Text style={styles.btnText}>Travel</Text>
         </TouchableHighlight>
         <TouchableWithoutFeedback>
@@ -26,8 +44,19 @@ export default function App() {
         </TouchableWithoutFeedback>
         <Pressable>
           <Text style={{ color: "white", fontSize: 25 }}>Press</Text>
-        </Pressable>
+        </Pressable> */}
       </View>
+      <TextInput
+        style={styles.input}
+        placeholder={isWork ? "Add a Todo" : "Where do you want to go?"}
+        onChangeText={onChangeText}
+        value={text}
+        // keyboardType="visible-password"
+        // returnKeyType="go"
+        // returnKeyLabel="zz"
+        // secureTextEntry
+        // multiline
+      />
     </View>
   );
 }
@@ -44,9 +73,15 @@ const styles = StyleSheet.create({
     marginTop: 100,
   },
   btnText: {
-    color: "white",
-    // color: theme.gray,
     fontSize: 40,
     // fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
